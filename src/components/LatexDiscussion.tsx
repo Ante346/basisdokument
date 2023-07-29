44583becfbb3dc64e38c55e5d82d18d4929f89db
Matthias <matthias@antholzer.dev>
2023-07-29 15:49:23 +0200

    imported latex.js, parsing does not work correctly

--- src/components/LatexDiscussion.tsx
+++ src/components/LatexDiscussion.tsx
@@ -1,10 +1,35 @@
+import { parse, HtmlGenerator } from 'latex.js'
 
+let latex = "\documentclass{article} \title{test} \author{Matthias Antholzer} \begin{document} \maketitle{} Hi, this is a line of text. \end{document}"
+
+let generator = new HtmlGenerator({ hyphenate: false })
+
+let doc = parse(latex, { generator: generator })
+
+console.log(doc)
 
 export const LatexDiscussion = () => {
 
     return (
+        
+        <div className="flex flew-row justify-around gap-4">
 
-        <div>Test</div>
+            <div>
+
+                <h1>Editor</h1>
+                <textarea></textarea>
+
+            </div>
+
+            <div>
+
+                <h1>Vorschau</h1>
+                <div>{doc.htmlDocument().getElementsByTagName("p")[0].innerHTML}</div>
+
+            </div>
+
+        </div>
+        
 
     );
 }
